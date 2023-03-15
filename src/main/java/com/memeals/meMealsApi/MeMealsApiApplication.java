58dad5a42f4d8aacea08d3c2c2e194a6da9d804d
Nate <npieraut@live.fr>
2023-03-14 21:01:33 -0400

    first commit

---
+++ src/main/java/com/memeals/meMealsApi/MeMealsApiApplication.java
@@ -0,0 +1,13 @@
+package com.memeals.meMealsApi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeMealsApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MeMealsApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/memeals/meMealsApi/MeMealsApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.memeals.meMealsApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeMealsApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
