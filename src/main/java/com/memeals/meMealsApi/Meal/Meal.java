83c3103615fbcf7b647b376afc822496f68e87e6
flopiti <npieraut@live.fr>
2023-03-17 19:24:49 -0400

    Meal

---
+++ src/main/java/com/memeals/meMealsApi/Meal/Meal.java
@@ -0,0 +1,65 @@
+package com.memeals.meMealsApi.Meal;
+
+import javax.persistence.*;
+import java.util.List;
+
+@Entity
+@Table(name = "meals")
+public class Meal {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "meal_name")
+    private String mealName;
+
+    @Column(name = "icon_url")
+    private String iconUrl;
+
+    @ElementCollection
+    @CollectionTable(name = "meal_ingredients", joinColumns = @JoinColumn(name = "meal_id"))
+    @Column(name = "ingredient")
+    private List<String> ingredients;
+
+    public Meal() {
+    }
+
+    public Meal(String mealName, String iconUrl, List<String> ingredients) {
+        this.mealName = mealName;
+        this.iconUrl = iconUrl;
+        this.ingredients = ingredients;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getMealName() {
+        return mealName;
+    }
+
+    public void setMealName(String mealName) {
+        this.mealName = mealName;
+    }
+
+    public String getIconUrl() {
+        return iconUrl;
+    }
+
+    public void setIconUrl(String iconUrl) {
+        this.iconUrl = iconUrl;
+    }
+
+    public List<String> getIngredients() {
+        return ingredients;
+    }
+
+    public void setIngredients(List<String> ingredients) {
+        this.ingredients = ingredients;
+    }
+}
