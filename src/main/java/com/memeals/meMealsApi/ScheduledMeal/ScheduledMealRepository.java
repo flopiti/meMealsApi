c33f5764164747e498662517ed05672068293502
flopiti <npieraut@live.fr>
2023-03-18 17:36:53 -0400

    refa

--- src/main/java/com/memeals/meMealsApi/ScheduledMeal/ScheduledMealRepository.java
+++ src/main/java/com/memeals/meMealsApi/ScheduledMeal/ScheduledMealRepository.java
@@ -1,9 +1,6 @@
 package com.memeals.meMealsApi.ScheduledMeal;
-import java.util.List;
-import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-
 
 public interface ScheduledMealRepository extends JpaRepository<ScheduledMeal, Long> {
 
