019e8be2425ef933245326d8c46d4d37b5e08022
flopiti <npieraut@live.fr>
2023-03-18 17:34:51 -0400

    long exception

--- src/main/java/com/memeals/meMealsApi/MealNotFoundException.java
+++ src/main/java/com/memeals/meMealsApi/MealNotFoundException.java
@@ -4,4 +4,7 @@ public class MealNotFoundException extends RuntimeException {
     public MealNotFoundException(String message) {
         super(message);
     }
+    public MealNotFoundException(Long mealId) {
+        super("Meal not found with id : " + mealId);
+    }
 }
