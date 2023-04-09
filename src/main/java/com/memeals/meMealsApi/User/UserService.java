5c35d6df871b4fb87b51876d5dfbf145f53e2508
flopiti <npieraut@live.fr>
2023-04-09 17:46:43 -0400

    user

---
+++ src/main/java/com/memeals/meMealsApi/User/UserService.java
@@ -0,0 +1,36 @@
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+
+    private final UserRepository userRepository;
+
+    @Autowired
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public User createUser(User user) {
+        // Check if a user with the same Auth0 ID already exists
+        if (userRepository.existsByAuth0Id(user.getAuth0Id())) {
+            throw new IllegalStateException("User with the same Auth0 ID already exists.");
+        }
+
+        // Check if a user with the same username already exists
+        if (userRepository.existsByUsername(user.getUsername())) {
+            throw new IllegalStateException("Username is already taken.");
+        }
+
+        // Check if a user with the same email already exists
+        if (userRepository.existsByEmail(user.getEmail())) {
+            throw new IllegalStateException("Email is already taken.");
+        }
+
+        return userRepository.save(user);
+    }
+
+    public User getUserByAuth0Id(String auth0Id) {
+        return userRepository.findByAuth0Id(auth0Id);
+    }
+}
