6908c97fa9041f5bd50bfc0c38dba5c7068a1a01
flopiti <npieraut@live.fr>
2023-04-22 14:44:57 -0400

    save user

--- src/main/java/com/memeals/meMealsApi/User/UserService.java
+++ src/main/java/com/memeals/meMealsApi/User/UserService.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import java.util.Optional;
 
+
 @Service
 public class UserService {
 
@@ -42,8 +43,11 @@ public class UserService {
         user.setEmail(email);
         user.setAuth0Id(auth0Id);
         user.setUsername(username);
-
-        return userRepository.save(user);
+        try {
+            return userRepository.save(user);
+        } catch (Exception e) {
+            return null;
+        }
     }
 
     public User edit(User user) {
