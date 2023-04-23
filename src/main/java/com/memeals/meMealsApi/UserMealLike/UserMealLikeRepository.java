f72bb84434c2bdf3c844c29829e545406ba9b017
flopiti <npieraut@live.fr>
2023-04-23 13:07:42 -0400

    by by user and meal

--- src/main/java/com/memeals/meMealsApi/UserMealLike/UserMealLikeRepository.java
+++ src/main/java/com/memeals/meMealsApi/UserMealLike/UserMealLikeRepository.java
@@ -4,11 +4,14 @@ import com.memeals.meMealsApi.User.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import java.util.List;
+import com.memeals.meMealsApi.Meal.Meal;
 
 
 @Repository
 public interface UserMealLikeRepository extends JpaRepository<UserMealLike, Long> {
 
     List<UserMealLike> findByUser(User user);
+
+    UserMealLike findByUserAndMeal(User user, Meal meal);
 
 }
