6aecdeb362a1a6740eb11fa07cb944390a7a2c29
flopiti <npieraut@live.fr>
2023-04-26 17:21:58 -0400

    controller

---
+++ src/main/java/com/memeals/meMealsApi/Ingredient/IngredientController.java
@@ -0,0 +1,49 @@
+package com.memeals.meMealsApi.Ingredient;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/ingrdients")
+public class IngredientController {
+
+    @Autowired
+    private IngredientService ingredientService;
+
+    @PostMapping
+    public ResponseEntity<Ingredient> createIngredient(@RequestBody Ingredient ingredient) {
+        Ingredient savedIngredient = ingredientService.saveIngredient(ingredient);
+        return new ResponseEntity<>( savedIngredient, HttpStatus.CREATED);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Ingredient> getIngredientById(@PathVariable Long id) {
+        Ingredient ingredient = ingredientService.getIngredientById(id);
+        if (ingredient != null) {
+            return new ResponseEntity<>(ingredient, HttpStatus.OK);
+        }
+        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Ingredient>> getAllIngredients() {
+        List<Ingredient> savedIngredient = ingredientService.getAllIngredients();
+        return new ResponseEntity<>(savedIngredient, HttpStatus.OK);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteIngredient(@PathVariable Long id) {
+        ingredientService.deleteIngredient(id);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    }
+}
