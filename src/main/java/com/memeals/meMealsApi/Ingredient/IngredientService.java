50bcfffc29a3a772e58b49e0fb2acee46e3b1267
flopiti <npieraut@live.fr>
2023-04-26 17:22:04 -0400

    service

---
+++ src/main/java/com/memeals/meMealsApi/Ingredient/IngredientService.java
@@ -0,0 +1,14 @@
+package com.memeals.meMealsApi.Ingredient;
+
+import java.util.List;
+
+public interface IngredientService {
+    
+    Ingredient saveIngredient(Ingredient ingredient);
+
+    Ingredient getIngredientById(Long id);
+
+    List<Ingredient> getAllIngredients();
+
+    void deleteIngredient(Long id);
+}
