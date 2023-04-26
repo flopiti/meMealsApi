b8369a143b661b98cf4337cb1f13d51ec6ce9315
flopiti <npieraut@live.fr>
2023-04-26 15:45:13 -0400

    prettier

--- src/main/java/com/memeals/meMealsApi/ScheduledMeal/ScheduledMealRequest.java
+++ src/main/java/com/memeals/meMealsApi/ScheduledMeal/ScheduledMealRequest.java
@@ -1,21 +1,24 @@
 package com.memeals.meMealsApi.ScheduledMeal;
 
 import java.time.LocalDate;
-
 import org.springframework.format.annotation.DateTimeFormat;
-
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-
-
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 public class ScheduledMealRequest {
+
     private Long userId;
     private Long mealId;
     @DateTimeFormat(iso = DateTimeFormat.ISO.DATE)
     private LocalDate date;
-    private MealType mealType;
+    private ScheduledMealType mealType;
+
+    public ScheduledMealRequest(Long mealId, LocalDate date, ScheduledMealType mealType) {
+        this.mealId = mealId;
+        this.date = date;
+        this.mealType = mealType;
+    }
 }
