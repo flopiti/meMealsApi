9bca2286455d7e16f3f9e2d388fbf4bfca78ca98
flopiti <npieraut@live.fr>
2023-04-26 15:44:06 -0400

    prettier

--- src/main/java/com/memeals/meMealsApi/AuthService.java
+++ src/main/java/com/memeals/meMealsApi/AuthService.java
@@ -4,21 +4,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.Authentication;
-import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationToken;
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.interfaces.DecodedJWT;
-import org.springframework.security.oauth2.server.resource.authentication.BearerTokenAuthentication;
-import org.springframework.security.oauth2.core.DefaultOAuth2AuthenticatedPrincipal;
 import com.auth0.spring.security.api.authentication.AuthenticationJsonWebToken;
-import java.util.Map;
-import com.auth0.spring.security.api.authentication.AuthenticationJsonWebToken;
-import com.auth0.jwt.JWT;
-import com.auth0.jwt.interfaces.Claim;
 import com.memeals.meMealsApi.User.User;
 import com.memeals.meMealsApi.User.UserService;
+
 @Service
 public class AuthService {
-
   private final UserService userService;
 
   @Autowired
@@ -26,41 +19,39 @@ public class AuthService {
     this.userService = userService;
 
   }
+  public String getAuth0Id() {
+      Authentication authentification = SecurityContextHolder.getContext().getAuthentication();  
+      if (authentification instanceof AuthenticationJsonWebToken) {
+          String token = authentification.getCredentials().toString();
+          DecodedJWT jwt = JWT.decode(token);
+          String id = jwt.getClaim("sub").asString().split("\\|")[1];            
+          if (id != null) {;
+            return id;
+          } else {
+              System.out.println("User ID not found in the JWT token");
+            return null;
+          }
+      }
+      return null;
+  }
 
-
-    public String getAuth0Id() {
-        Authentication authentification = SecurityContextHolder.getContext().getAuthentication();  
-        if (authentification instanceof AuthenticationJsonWebToken) {
-            String token = authentification.getCredentials().toString();
-            DecodedJWT jwt = JWT.decode(token);
-            String id = jwt.getClaim("sub").asString().split("\\|")[1];            
-            if (id != null) {;
-              return id;
-            } else {
-                System.out.println("User ID not found in the JWT token");
-              return null;
-            }
-        }
-        return null;
-    }
-
-    public User getUser() {
-        Authentication authentification = SecurityContextHolder.getContext().getAuthentication();  
-        if (authentification instanceof AuthenticationJsonWebToken) {
-            String token = authentification.getCredentials().toString();
-            DecodedJWT jwt = JWT.decode(token);
-            String id = jwt.getClaim("sub").asString().split("\\|")[1];            
-            if (id != null) {
-              User user = userService.getUserByAuth0Id(id).get();
-              System.out.println(user.toString());
-              return user;
-            } else {
-                System.out.println("User ID not found in the JWT token");
-              return null;
-            }
-        }
-        return null;
-    }                         
+  public User getUser() {
+      Authentication authentification = SecurityContextHolder.getContext().getAuthentication();  
+      if (authentification instanceof AuthenticationJsonWebToken) {
+          String token = authentification.getCredentials().toString();
+          DecodedJWT jwt = JWT.decode(token);
+          String id = jwt.getClaim("sub").asString().split("\\|")[1];            
+          if (id != null) {
+            User user = userService.getUserByAuth0Id(id).get();
+            System.out.println(user.toString());
+            return user;
+          } else {
+              System.out.println("User ID not found in the JWT token");
+            return null;
+          }
+      }
+      return null;
+  }                         
 
   public String getUserId(){
     String auth0Id = getAuth0Id();
