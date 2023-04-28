c71c6144b1285a693d81054c5fda723204d98f67
flopiti <npieraut@live.fr>
2023-04-27 22:13:32 -0400

    service

--- src/main/java/com/memeals/meMealsApi/Meal/MealServiceImpl.java
+++ src/main/java/com/memeals/meMealsApi/Meal/MealServiceImpl.java
@@ -3,6 +3,9 @@ package com.memeals.meMealsApi.Meal;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.memeals.meMealsApi.Ingredient.Ingredient;
+import com.memeals.meMealsApi.Ingredient.IngredientRepository;
+
 import java.util.List;
 import java.util.Optional;
 
@@ -12,9 +15,25 @@ public class MealServiceImpl implements MealService {
     @Autowired
     private MealRepository mealRepository;
 
+    private IngredientMealRepository mealIngredientRepository;
+
+    private IngredientRepository ingredientRepository;
+
     @Override
-    public Meal saveMeal(Meal meal) {
-        return mealRepository.save(meal);
+    public Meal saveMeal(MealDTO mealDTO) {
+        Meal meal = new Meal(mealDTO.getMealName(), mealDTO.getIconUrl());
+        meal =  mealRepository.save(meal);
+        if (mealDTO.getMealIngredients() != null) {
+            for (MealIngredientDTO mealIngredientDTO : mealDTO.getMealIngredients()) {
+                Ingredient ingredient = ingredientRepository.findById(mealIngredientDTO.getIngredientId())
+                        .orElseThrow(() -> new IllegalArgumentException("Invalid ingredient ID"));
+
+                IngredientMeal mealIngredient = new IngredientMeal(null, meal, ingredient, mealIngredientDTO.getQuantity(),mealIngredientDTO.getUnitOfMeasurement());
+                mealIngredientRepository.save(mealIngredient);
+            }
+        }
+
+        return meal;
     }
 
     @Override
