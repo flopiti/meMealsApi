32dfe1c08e046457a9db048931e7d7d0d2d4c4dc
flopiti <npieraut@live.fr>
2023-04-27 22:13:36 -0400

    dto

--- src/main/java/com/memeals/meMealsApi/Meal/MealService.java
+++ src/main/java/com/memeals/meMealsApi/Meal/MealService.java
@@ -3,7 +3,7 @@ package com.memeals.meMealsApi.Meal;
 import java.util.List;
 
 public interface MealService {
-    Meal saveMeal(Meal meal);
+    Meal saveMeal(MealDTO meal);
     Meal getMealById(Long id);
     List<Meal> getAllMeals();
     Meal updateMeal(Meal meal);
