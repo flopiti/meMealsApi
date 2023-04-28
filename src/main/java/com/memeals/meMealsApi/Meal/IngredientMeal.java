9b8ba9bfef8e5cd54c35574d501affe021349515
flopiti <npieraut@live.fr>
2023-04-27 23:18:02 -0400

    no args

--- src/main/java/com/memeals/meMealsApi/Meal/IngredientMeal.java
+++ src/main/java/com/memeals/meMealsApi/Meal/IngredientMeal.java
@@ -5,7 +5,9 @@ import com.memeals.meMealsApi.Ingredient.Ingredient;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@NoArgsConstructor
 @Data
 @Entity
 @Table(name = "meal_ingredients")
