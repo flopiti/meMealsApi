a60ec0aa2249ddd6ba978db87a8373b23e1ea17e
Nathan <npieraut@live.fr>
2023-08-20 00:07:02 -0400

    repo

--- src/main/java/com/memeals/meMealsApi/ScheduledMeal/ScheduledMealRepository.java
+++ src/main/java/com/memeals/meMealsApi/ScheduledMeal/ScheduledMealRepository.java
@@ -1,9 +1,13 @@
 package com.memeals.meMealsApi.ScheduledMeal;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.time.LocalDate;
 import java.util.List;
 
 public interface ScheduledMealRepository extends JpaRepository<ScheduledMeal, Long> {
     List<ScheduledMeal> findByUserId(Long userId);
+
+    List<ScheduledMeal> findByUserIdAndDateBetween(Long userId, LocalDate startDate, LocalDate endDate);
 
 }
